ba8c3e9146de51dc7abf7c8b4cb7a8cfcc834f2f
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-11-23 23:17:52 +0530

    Maximum element of A Raw Data

---
+++ Descriptive Analysis/maximum.R
@@ -0,0 +1,27 @@
+readInput<-function(){
+	j<-0
+	n<-readline("size : ")
+	array<-as.numeric()
+	while(j<n) {
+		y<-as.numeric(readline("Enter Data : "))
+		array<-as.numeric(c(array,y))
+		j<-j+1
+	}
+	return array
+}
+
+#This function Calculates Maximum element of An Array
+Maximum<-function(array) {
+	total<-length(array)
+	temp<-array[1]
+	for ( i in 2:total ) {
+		if ( array[i] > temp ) {
+			temp <-array[i]
+		}
+	}
+	return temp
+}
+
+array<-readInput()
+
+Maximum(array)
