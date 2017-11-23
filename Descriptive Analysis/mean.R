ce75ef6f18424ba16c2a11613755d922fd42e3fc
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-11-23 23:18:17 +0530

    Mean of A Raw Data

---
+++ Descriptive Analysis/mean.R
@@ -0,0 +1,18 @@
+readInput<-function() {
+	j<-0
+	n<-readline("size : ")
+	array<-as.numeric()
+	while(j<n) {
+		y<-as.numeric(readline("Enter Data : "))
+		array<-as.numeric(c(array,y))
+		j<-j+1
+	}
+	return array
+}
+
+Mean<-function(array) {
+	return (sum(array)/length(array))
+}
+
+array<-readInput()
+Mean(array)
