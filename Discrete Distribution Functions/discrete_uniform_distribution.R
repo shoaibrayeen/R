f09c88277b67ea0e8597d2d0de4547b4882d51d0
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-11-27 21:44:14 +0530

    P(X=x1) = P(X=x2) = ... = P(X=xn)

---
+++ Discrete Distribution Functions/discrete_uniform_distribution.R
@@ -0,0 +1,13 @@
+
+discrete_uniform<-function(n) {
+	return(1/n)
+}
+
+#In probability theory and statistics, the discrete uniform distribution is a symmetric probability distribution 
+#whereby a finite number of values are equally likely to be observed; every one of n values has equal probability 1/n.
+#Another way of saying "discrete uniform distribution" would be "a known, finite number of outcomes equally likely to happen".
+
+
+n<-readline("Total Values : ")
+
+discrete_uniform(n)
