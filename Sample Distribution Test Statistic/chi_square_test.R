f82b57e6bcfdd4a4543f0e9a0a7f00ee6b1ff7c2
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-12-02 21:10:30 +0530

    Chi Square Test

---
+++ Sample Distribution Test Statistic/chi_square_test.R
@@ -0,0 +1,24 @@
+#if it's one tail , pass alpha/2 while calling the function and ask to user regarding one tail things
+#need to calculate sample variance to compute chisquare value
+
+chiSquaretest<-function(sVariance , pVariance ,n ,alpha,flag) {
+	df<-ln-1
+	chi<-df*sVariance/pVarianc
+	pvalue<-pchisq(chi,df)
+	if(flag==0) {
+		pvalue<-2*pvalue
+	}
+	if(pvalue < alpha ) {
+			return("Reject H0") #reject null hypothests
+		}
+	return("Accept H0")	#accept H0
+}
+
+
+sVariance<-readline("Sample Variance : ")
+pVariance<-readline("Population Variance for H0 : ")
+n<-readline("Sample Size : ")
+alpha<-readline("Level of Significance : ")
+flag<-readline("0 if two tail otherwise anything : ")
+
+chiSquaretest(sVariance , pVariance ,n ,alpha,flag) 
