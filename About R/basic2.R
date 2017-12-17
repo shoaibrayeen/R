bf2464222ff3ea8c765b16371eac6f0fdddba11b
MOHD SHOAIB RAYEEN <shoaib.mca17.du@gmail.com>
2017-12-17 17:32:45 +0530

    assign , add , remove and functions

---
+++ About R/basic2.R
@@ -0,0 +1,27 @@
+#Well you are learning a new language, you got to perform the ritual, say "Hello World!"
+print("Hello World")
+
+#Print the square root of 2
+sqrt(2)
+
+#Now there's a hard one, set a variable and use it!
+a <- 2
+a=2
+2 ->a
+
+#Have a look at the variables in your (work)space
+ls()
+.abc <- 2
+ls()
+
+#Are you sure they are the only ones, noone hiding behind? 
+ls(all.names=TRUE)
+
+#What's in the name? Give some more information!
+ls.str()
+
+#Save confusion, save memory, learn to remove variables when not needed. 
+rm()
+
+#Be smart, you cannot keep writing the whole thing repeatedly, make a function!
+f1 <- function(a,b) print(a+b)
